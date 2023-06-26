d3c6f8824e9fd46efbb9d1394752d9a8734f76e6
hfgd <jan.lunz@web.de>
2023-06-26 21:20:59 +0200

    fixed saving bug
    Bugfix

--- manager/levelManager.gd
+++ manager/levelManager.gd
@@ -3,16 +3,12 @@ extends Node
 var current_level = ""
 var menu_state = false
 var system_nodes = []
-var level_nodes = []
 
 # Jan + Lea
 func _ready():
 	system_nodes.append(get_node("player"))
 	system_nodes.append(get_node("Camera2D"))
 	system_nodes.append(get_node("userInterface"))
-	for i in get_children():
-		if !system_nodes.has(i):
-			level_nodes.append(i)
 	set_current_level("startMenu", true, 0, 0)
 
 # Jan + Lea
@@ -27,9 +23,8 @@ func get_menu_state():
 func set_current_level(path, is_menu, x, y):
 	current_level = path
 	menu_state = is_menu
-	for i in level_nodes:
-		if i.is_visible():
-			i.hide()
+	for i in get_children():
+		i.hide()
 	get_node(path).show()
 	if is_menu:
 		for i in system_nodes:
